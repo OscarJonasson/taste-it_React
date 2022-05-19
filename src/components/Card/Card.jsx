a277b3de532d2c256937279136f61582f10c88ce
Jonasson Oscar <oscjjonasson@gmail.com>
2022-05-19 20:28:17 +0300

    Outside link conditional rendering

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,17 @@
 import { Link } from 'react-router-dom';
 import classes from './Card.module.css';
 function Card(props) {
+  if (props.link.includes('https')) {
+    return (
+      <div className={classes.card}>
+        <h3>{props.title}</h3>
+        <p className={classes.card__desc}>{props.message}</p>
+        <a href={props.link} target="_blank">
+          {props.linkName}
+        </a>
+      </div>
+    );
+  }
   return (
     <div className={classes.card}>
       <h3>{props.title}</h3>
